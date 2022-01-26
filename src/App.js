69c7783e6a0de1d342a53c137fc93c5d45602732
JoyAnneW <joyawilliams@gmail.com>
2022-01-26 18:46:49 +0100

    created SearchFilter component and began styling it. updated font color of Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import SearchFilter from "./components/SearchFilter";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<main>
+				<SearchFilter />
+			</main>
 		</div>
 	);
 }
---
+++ src/components/SearchFilter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { AiOutlineSearch, AiOutlineDown } from "react-icons/ai";
+
+export default function SearchFilter() {
+	return (
+		<>
+			<form action="" className="form">
+				<div className="search">
+					<AiOutlineSearch className="search--icon" />
+					<input
+						className="search--input"
+						type="search"
+						placeholder="Search for a country..."
+					/>
+				</div>
+			</form>
+			<div className="dropdown--container">
+				<label htmlFor="regions"></label>
+				<select name="regions" id="regions" className="dropdown--options">
+					<option value="" selected>
+						Filter by Region
+					</option>
+
+					<option value="Africa">Africa</option>
+					<option value="America">America</option>
+					<option value="Asia">Asia</option>
+					<option value="Europe">Europe</option>
+					<option value="Oceania">Oceania</option>
+				</select>
+				<AiOutlineDown className="dropdown--icon" />
+			</div>
+		</>
+	);
+}
