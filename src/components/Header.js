c8941f0ed583a3f908f16ce2ee3fee964da1fb19
JoyAnneW <joyawilliams@gmail.com>
2022-01-26 13:40:29 +0100

    styled the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { IoMoonOutline } from "react-icons/io5";
+import { BsMoon } from "react-icons/bs";
 
 export default function Header() {
 	return (
 		<header>
 			<h1 className="header--main-heading">Where in the world?</h1>
 			<div className="header__theme-toggler">
-				<IoMoonOutline />
+				<BsMoon />
 				<span>Dark/Light Mode</span>
 			</div>
 		</header>
