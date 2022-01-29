2bebb11bbc65c6b98ef16a883dc7b8600c9bdd14
JoyAnneW <joyawilliams@gmail.com>
2022-01-29 21:12:31 +0100

    added toggle effect to dropdown so that click opens and closes it.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineDown } from "react-icons/ai";
 export const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
 //for now putting regions outside of function until I do the API call
 //destructuring props as a parameter.
-export default function Dropdown({ regions }) {
+
+// this component can be made more generic so that it can be used in different contexts outside of this project
+
+export default function Dropdown({ regions, value, onChange }) {
+	// we need to use state to track whether the Dropdown is open or not
+
+	const [open, setOpen] = useState(false);
+
 	const region = regions.map((region) => (
 		<div className="option">{region}</div>
 	));
+
 	return (
 		<div className="dropdown">
-			<div className="control">
+			<div
+				className="control"
+				onClick={() => setOpen((prevState) => !prevState)}
+			>
 				<div className="selected-value">Filter by Region</div>
-				<div className="arrow">
-					{" "}
+				<div className={`arrow ${open ? "open" : null}`}>
 					<AiOutlineDown className="dropdown--icon" />
 				</div>
 			</div>
-			<div className="options">{region} </div>
+			<div className={`options ${open ? "open" : null}`}>{region}</div>
 		</div>
 	);
 }
